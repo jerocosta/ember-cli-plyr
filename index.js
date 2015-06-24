e40e0da3c632f51523a0758041296fa8accb9afc
Louis R <louisrudner@gmail.com>
2015-06-24 15:32:30 +1000

    Fixes index.js multi-line string. Closes #2, closes #3.

--- index.js
+++ index.js
@@ -12,23 +12,24 @@ module.exports = {
     app.import(app.bowerDirectory + '/plyr/dist/plyr.css');
     app.import(app.bowerDirectory + '/plyr/dist/sprite.svg', { destDir: 'sprites' });
   },
-  contentFor: function(type, config) {
+  contentFor: function(type) {
     if (type === 'body') {
       // Load SVG icons for video player.
-      return '<script>\
-              (function(d,p){\
-                  var a=new XMLHttpRequest(),\
-                      b=d.body;\
-                  a.open("GET",p,!0);\
-                  a.send();\
-                  a.onload=function(){\
-                      var c=d.createElement("div");\
-                      c.style.display="none";\
-                      c.innerHTML=a.responseText;\
-                      b.insertBefore(c,b.childNodes[0])\
-                  }\
-              })(document,"sprites/sprite.svg");\
-              </script>';
+      return "<script>" +
+              "(function(d,p){" +
+                  "var a=new XMLHttpRequest()," +
+                      "b=d.body;" +
+                  "a.open(\"GET\",p,!0);" +
+                  "a.send();" +
+                  "a.onload=function(){" +
+                      "var c=d.createElement(\"div\");" +
+                      "c.style.display=\"none\";" +
+                      "c.innerHTML=a.responseText;" +
+                      "b.insertBefore(c,b.childNodes[0])" +
+                  "}" +
+              "})(document,\"sprites/sprite.svg\");" +
+
+              "</script>";
     }
   }
 };
